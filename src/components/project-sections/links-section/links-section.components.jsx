b40fd9351884ea1dd7663213702a99a48cbdc1d9
Mahmoud Abd Elghany <17260098+Mahmoud-Abd-Elghany@users.noreply.github.com>
2021-12-31 17:50:56 +0200

    fixed project demo link

--- src/components/project-sections/links-section/links-section.components.jsx
+++ src/components/project-sections/links-section/links-section.components.jsx
@@ -5,8 +5,8 @@ function LinksSection({expanded, links: {github, demo}}) {
     
     return (
         <div className={expanded? 'links-section expanded' : 'links-section'} target="_blank">
-                <a className='icon-custom' href={demo}>
-                    <i class="fas fa-desktop fa-2x" target="_blank" rel="noopener noreferrer"></i>
+                <a className='icon-custom' href={demo} target="_blank" rel="noopener noreferrer">
+                    <i class="fas fa-desktop fa-2x" ></i>
                 </a>
                 <a className='icon-custom' href={github} target="_blank" rel="noopener noreferrer">
                     <i class="fab fa-github fa-2x"></i>
