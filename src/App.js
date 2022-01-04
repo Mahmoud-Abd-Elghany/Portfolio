d80e01b27841fda6cf837a141fa44b456207f4ad
Mahmoud Abdelghany <mazpc97@gmail.com>
2022-01-05 00:24:23 +0200

    Finishing touches

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
       <div className ='App'>
             <Header/>
             <Container/>
-            <div className="contribute">Photo by <a href="https://unsplash.com/@pawel_czerwinski?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pawel Czerwinski</a> on <a href="https://unsplash.com/s/photos/abstract?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></div>
-  
+            {/* <div className="contribute">Photo by <a href="https://unsplash.com/@pawel_czerwinski?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Pawel Czerwinski</a> on <a href="https://unsplash.com/s/photos/abstract?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a></div> */}
       </div>
     </Router>
   );
